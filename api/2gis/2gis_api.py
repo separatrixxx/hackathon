0d814e4dabc46629185fa63e96c8b5b04b18b408
biggvladek <zuevvlad55@gmail.com>
2022-09-02 00:01:00 +0300

    2gis_api

---
+++ api/2gis/2gis_api.py
@@ -0,0 +1,35 @@
+import requests
+
+
+city = 'Санкт-Петербург'
+Key = 'rugoqt'
+
+class two_gis():
+
+    def get_lakes(self):
+        pass
+
+
+    def get_place(self):
+        pass
+
+    def get_base(self):
+        pass
+
+
+    def get_food(self):
+        pass
+
+
+    def get_hostel(self):
+        pass
+
+
+
+    def get_petrol(self):
+
+        pass
+
+
+
+
