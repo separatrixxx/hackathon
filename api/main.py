8e785219ef85f8740ed9b6fa6c078e5687016755
biggvladek <zuevvlad55@gmail.com>
2022-09-01 22:41:51 +0300

    test_render_map

--- api/main.py
+++ api/main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
-from routers import city
+from routers import map
+from fastapi.staticfiles import StaticFiles
 
+from fastapi.templating import Jinja2Templates
 app = FastAPI()
-app.include_router(city.router, prefix ='/api/v1/city', tags=['city'])
+templates = Jinja2Templates(directory="static")
+
+
+
+app.include_router(map.router, prefix ='/api/v1/city', tags=['city'])
