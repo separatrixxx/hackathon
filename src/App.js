9a203ef85f762e26530f306ca6ea4ae0c2de15a5
Nikita Lokhmatov <nikitalochmatov@gmail.com>
2022-09-02 20:57:38 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function App() {
   return (
-    <div className="">
-
+    <div className="flex justify-center w-full h-screen">
+      <div id="map" className="w-full h-screen"></div>
     </div>
   );
 }
