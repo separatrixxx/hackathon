cb929ce544a4df1925105a3955b76e1e6fbccdf0
biggvladek <zuevvlad55@gmail.com>
2022-09-03 18:52:39 +0300

    нафигация

--- api/2gis/2gis_api.py
+++ api/2gis/2gis_api.py
@@ -3,16 +3,15 @@ import requests
 
 
 class two_gis():
+    def __init__(self):
+        self.key = 'rugoqt4514'
 
-    def get_lakes(self):
-        pass
+    def get_item(self,item,coordinates):   # Нужно оставить только id
+        response = requests.get(f'https://catalog.api.2gis.com/3.0/items?q={item}&sort_point=59.942311,30.266461&key={self.key}')
+        return response
 
 
-    def get_place(self):
-        pass
 
-    def get_base(self):
-        pass
 
 
     def get_food(self):
@@ -27,6 +26,25 @@ class two_gis():
     def get_petrol(self):
 
         pass
+
+
+
+A=two_gis()
+
+
+
+key = 'rugoqt4514'
+
+
+
+response = requests.get(f'https://catalog.api.2gis.com/2.0/catalog/rubric/search?q=кафе&region_id=38&key={key}')
+
+print(response.json())
+
+
+
+
+
 
 
 
