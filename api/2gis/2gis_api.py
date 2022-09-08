5d0a660c8afdc194f5d75105024d7be3cdcccf21
biggvladek <zuevvlad55@gmail.com>
2022-09-08 16:06:04 +0300

    dums

--- api/2gis/2gis_api.py
+++ api/2gis/2gis_api.py
@@ -6,15 +6,16 @@ class two_gis():
     def __init__(self):
         self.key = 'rugoqt4514'
 
-    def get_item(self,item,coordinates):   # Нужно оставить только id
-        response = requests.get(f'https://catalog.api.2gis.com/3.0/items?q={item}&sort_point=59.942311,30.266461&key={self.key}')
-        return response
+   # def get_item(self,item,coordinates):   # Нужно оставить только id
+   #     response = requests.get(f'https://catalog.api.2gis.com/3.0/items?q={item}&sort_point=59.942311,30.266461&key={self.key}')
+   #     return response
 
 
 
 
-
-    def get_food(self):
+# coordinates: 'x,y'
+    def get_food(self,coordinates):
+        #     response = requests.get(f'https://catalog.api.2gis.com/3.0/items?q=Кафе&sort_point=59.942311,30.266461{&key={self.key}')
         pass
 
 
@@ -37,15 +38,20 @@ key = 'rugoqt4514'
 
 
 
-response = requests.get(f'https://catalog.api.2gis.com/2.0/catalog/rubric/search?q=кафе&region_id=38&key={key}')
+# response = requests.get(f'https://catalog.api.2gis.com/2.0/region/search?q=Санкт-Петербург&key={key}')
+#
+# print(response.json())
+# response = requests.get(f'https://catalog.api.2gis.com/2.0/catalog/rubric/search?q=кафе&region_id=38&key={key}')
+#
+# print(response.json())
+#
+#
+#
+# response = requests.get(f'https://catalog.api.2gis.com/3.0/items?rubric_id=161&key={key}')
+#
+# print(response.json())
+
+
+response = requests.get(f'https://catalog.api.2gis.com/3.0/items?q= Ленинградская область кафе&sort_point=29.88209,59.87743&key={key}')
 
 print(response.json())
-
-
-
-
-
-
-
-
-
--- api/routers/map.py
+++ api/routers/map.py
@@ -13,3 +13,20 @@ async def render_map(request:Request):
     return templates.TemplateResponse('map.html',{"request": request})
 
 
+@router.get('/attractions',tags=['map'])
+async def get_attractions():
+    pass
+
+
+
+@router.get('/food',tags=['map'])
+async def get_food():
+    pass
+
+@router.get('/petrol',tags=['map'])
+async def get_petrol():
+    pass
+
+@router.get('/hostel',tags=['map'])
+async def get_hostel():
+    pass
