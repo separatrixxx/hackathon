e7b5ce8ee1614f847fe120e2cba3412102d3db0f
Nikita Lokhmatov <nikitalochmatov@gmail.com>
2022-09-22 13:16:12 +0300

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,9 @@ function Home() {
     return (
         <div className="flex flex-col w-full h-screen">
             <Header />
-            <div id="container" className="w-full h-5/6"></div>
+            <div className="h-5/6 mx-1 md:mx-5 rounded-xl overflow-hidden">
+                <div id="container" className="w-full h-screen"></div>
+            </div>
         </div>
     );
 }
